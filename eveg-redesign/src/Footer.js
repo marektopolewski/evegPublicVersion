72c372d0707a218f9f0cf6fdd6f0aee12aa5f207
Marek Topolewski <marek.topolewski@gmail.com>
2019-02-21 20:32:16 +0000

    fix the mess I made. Based on a0dac08fcfa1f9da8fa4e2cc22938dbbde28ede8, merged with 00a464f1ab317e3d954ebf19f9deab74aac324a2

--- eveg-redesign/src/Footer.js
+++ eveg-redesign/src/Footer.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react';
+import { FooterIcon, PayPalLogo, CardLogos } from './Asset';
 
 export default class Footer extends Component {
-  render(){
-    return (
-      <footer></footer>
-    );
-  }
+    render(){
+        return (
+            <footer className="footer">
+                <table className="footer-table"><tbody>
+                <tr>
+                    <td className="footer-small-td">
+                        <p><b>Helpful Links</b></p>
+                        <p><a href="/">FAQ</a></p>
+                        <p><a href="/">Click & Collect</a></p>
+                    </td>
+                    <td className="footer-large-td">
+                        <FooterIcon />
+                        <p className="copyright">@2014 InterVeg Coventry Ltd.</p>
+                        <PayPalLogo /> <CardLogos />
+                    </td>
+                    <td className="footer-small-td">
+                        <p><b>Contact</b></p>
+                        <div>
+                            University of Warwick <br/>
+                            CV4 7AL <br/>
+                            Coventry
+                        </div>
+                    </td>
+                </tr>
+                </tbody></table>
+            </footer>
+        );
+    }
 };
